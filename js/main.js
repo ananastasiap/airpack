ea14cfdef5c0c70aab4ac5fb1da8a43cdea909c0
ananastasiap <anestheziia@gmail.com>
2023-04-21 21:36:10 +0300

    feat: add timer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-import { tabs } from './modules';
+import { tabs } from './modules/tabs.js';
+import { timer } from './modules/timer.js';
 
 window.addEventListener('DOMContentLoaded', () => {
+  const deadline = '2023-05-30';
+
   tabs({
     headerSelector: '.slider',
     tabSelector: '.item__content',
     contentSelector: '.slider__content',
     activeClass: 'active'});
+  timer('.timer', deadline);
 });
